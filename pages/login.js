999141a9a51b79a7db9a1a9cbc8af511aa5ba10e
Kushal Gandhi <kushalgandhi2601@gmail.com>
2022-07-17 09:50:07 +0530

    temp commit

--- pages/login.js
+++ pages/login.js
@@ -7,7 +7,7 @@ const Login = ({ setloggedIn }) => {
   const [userDetails, setuserDetails] = useState({ username: "", phonenumber: "" })
   const [show, setshow] = useState(false)
   const [validpass, setvalidpass] = useState(false)
-  var pass = /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/
+  const pass = /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/
 
   const handleChange = (e) => {
     const { name, value } = e.target
